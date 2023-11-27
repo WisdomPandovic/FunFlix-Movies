42bd296f119277043221acafcc5503d865ee0126
WisdomPandovic <144015437+WisdomPandovic@users.noreply.github.com>
2023-11-27 04:10:12 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-     const apiKey = API_KEY;
+     const apiKey = window.API_KEY;
     const movieList = document.getElementById('movieList');
   
 
